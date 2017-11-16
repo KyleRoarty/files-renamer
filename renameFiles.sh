e10f4ed0aace5665e6d49a73a3e9768842d99060
Kyle <kyleroarty1716@gmail.com>
2017-11-16 14:22:25 -0600

    Arg parsing (#1)
    * Add getopts; fix what getopts breaks
    
    Use -i flag with a csl of indices.
    Removes last index automatically.
    Doesn't mv starting with '-' if only 1 index is specified
    README: Update example with new getopts
    
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

--- renameFiles.sh
+++ renameFiles.sh
@@ -1,19 +1,51 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]] ; then
-    echo 'You need arguments!'
-    exit 1
-fi
+show_help() {
+    echo "Usage: ToDo"
+}
+
+valid_arg() {
+    if [ -z "$1" ]; then
+        show_help
+        exit 1
+    else
+        echo "Valid variable"
+    fi
+}
 
 
-array=${@:1:$(($#-1))}
-len=${#array[@]}
+#array=${@:1:$(($#-1))}
+#len=${#array[@]}
+
+OPTIND=1
+
+indArr=()
+last=""
+indStr=""
+len=0
+
+while getopts "i:" opt; do
+    case "$opt" in
+    i)
+        indStr=$OPTARG
+        indArr=(${indStr//,/ })
+        last=${indArr[-1]}
+
+        unset 'indArr[${#arr[@]}-1]'
+        len=${#indArr[@]}
+        ;;
+    esac
+done
+
+echo $indArr
+valid_arg $indStr
 
 for f in *.*
 do
-    mv "$f"\
-       "$(awk -v var="${array[*]}" -v l="$len" -v num="${!#}" -F '[ .]'\
-           'BEGIN {split(var, varArr, / /)}
-            { for(x in varArr) printf "%s ",$varArr[x] }
-            { print "- " $num "." $NF }' <<< "$f")"
+    mv -- "$f"\
+          "$(awk -v var="${indArr[*]}" -v l="$len" -v num="$last" -F '[ .]'\
+             'BEGIN {split(var, varArr, / /)}
+              { for(x in varArr) printf "%s ",$varArr[x] }
+              { if(l != 0) {printf "- "} }
+              { printf "%s.%s",$num,$NF }' <<< "$f")"
 done
